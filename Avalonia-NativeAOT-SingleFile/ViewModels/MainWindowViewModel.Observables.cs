e2bd5b9a51252ccfd2d22e1757156b9895749bb4
peaceshi <peaceshi@outlook.com>
2026-04-20 01:58:55 +0800

    feat: add Light icon and toggle for dark mode in TitleBar; update transparency settings in window styles

--- Avalonia-NativeAOT-SingleFile/ViewModels/MainWindowViewModel.Observables.cs
+++ Avalonia-NativeAOT-SingleFile/ViewModels/MainWindowViewModel.Observables.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Styling;
+
 using CommunityToolkit.Mvvm.ComponentModel;
+
 using Models;
 
 namespace Avalonia_NativeAOT_SingleFile.ViewModels;
@@ -13,9 +17,17 @@ public partial class MainWindowViewModel
     [ObservableProperty] private double _materialOpacity;
     [ObservableProperty] private ObservableCollection<ViewModelBase> _pages = new([DefaultPage]);
     [ObservableProperty] private double _splitRectangleWidth = TitleBarHeight;
+    [ObservableProperty] private bool _isDarkMode;
 
     partial void OnIsWindowActiveChanged(bool value) => MaterialOpacity =
         value ? Constants.MaterialOpacityActivated : Constants.MaterialOpacityDeactivated;
 
     partial void OnIsPaneOpenChanged(bool value) => SplitRectangleWidth = value ? OpenPaneLength : TitleBarHeight;
+    partial void OnIsDarkModeChanged(bool value)
+    {
+        if (Application.Current is { } app)
+        {
+            app.RequestedThemeVariant = value ? ThemeVariant.Dark : ThemeVariant.Light;
+        }
+    }
 }
